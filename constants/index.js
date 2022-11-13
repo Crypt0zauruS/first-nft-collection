46aaea9eb67533d5ae4aa475ca6000d04ce5005b
Crypt0zauruS <101745417+Crypt0zauruS@users.noreply.github.com>
2022-11-13 12:33:21 +0400

    first commit

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,7 @@
 //export const NFT_CONTRACT_ADDRESS =
 // "0x720fdaAEd0067C162BfacAa580720b907d692869";
 export const NFT_CONTRACT_ADDRESS =
-  "0x03C82eef6FaE9c14B224e056c127a4155F47D404";
+  "0x8874fD48335576F89b2dB7927A5c1d7C7d3BC559";
 export const abi = [
   {
     inputs: [
